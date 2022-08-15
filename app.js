81bc4cfca64a87399d6a3ebc027d19a935dfab9b
110nard0 <76947677+110nard0@users.noreply.github.com>
2022-08-15 14:01:44 +0100

    Enable score to update on button click

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-const scoreOne = document.querySelector("#one");
-const scoreTwo = document.querySelector("#two");
+const scoreOne = document.querySelector('#one');
+const scoreTwo = document.querySelector('#two');
+
+const buttonOne = document.querySelector('#playerOne')
+const buttonTwo = document.querySelector('#playerTwo')
+const buttonThree = document.querySelector('#reset')
+
+let newScoreOne = 0;
+let newScoreTwo = 0;
+
+
+//select highest possible score playable 
+const highScore =
+
+	//add click event to player 1 score
+	buttonOne.addEventListener('click', () => {
+		newScoreOne++;
+		scoreOne.innerText = newScoreOne;
+	})
+
+//add click event to player 2 score
+buttonTwo.addEventListener('click', () => {
+	newScoreTwo++;
+	scoreTwo.innerText = newScoreTwo;
+})
+
+buttonThree.addEventListener('click', () => {
+	console.log("sup");
+})
